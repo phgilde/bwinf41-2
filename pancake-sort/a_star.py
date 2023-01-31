d12ebf83a640a5ec4ca74e1e13679814e28a12b9
phgilde <ph.gilde@gmail.com>
2023-01-31 11:37:14 +0100

    pancakesort solution

---
+++ pancake-sort/a_star.py
@@ -0,0 +1,28 @@
+from queue import PriorityQueue
+
+# finds the shortest path from start node to a node that fullfills target_pred. returns the path
+def a_star(start_node, target_pred, adj_func, cost_func, heur_func):
+    i = 0
+    queue = PriorityQueue()
+    queue.put((0, heur_func(start_node), i, start_node))
+    prev = {start_node: None}
+    cost = {start_node: 0 + heur_func(start_node)}
+    while not queue.empty():
+        _, _, _, node = queue.get()
+        if target_pred(node):
+            return reconstruct_path(node, prev)
+        for adj_node in adj_func(node):
+            new_cost = cost[node] - heur_func(node) + cost_func(node, adj_node) + heur_func(adj_node)
+            if adj_node not in cost or new_cost < cost[adj_node]:
+                i -= 1
+                cost[adj_node] = new_cost
+                queue.put((new_cost, heur_func(node), i, adj_node))
+                prev[adj_node] = node
+
+def reconstruct_path(node, prev):
+    path = [node]
+    while prev[node] is not None:
+        node = prev[node]
+        path.append(node)
+    return list(reversed(path))
+
---
+++ pancake-sort/least_flips.py
@@ -0,0 +1,73 @@
+import math
+from a_star import a_star
+
+# Pfannkuchenstapel umdrehen und Pfannkuchen essen.
+# Zeitkomplexität: O(k), weil k elemente verändert werden.
+def flip(arr, k):
+    return arr[: k - 1][::-1] + arr[k:]
+
+
+# Gibt alle möglichen nächsten Reihenfolgen zurück.
+# Zeitkomplexität: O(n^2), weil flip() O(n) ist und wir n-1 mal aufrufen.
+def next_arrs(arr):
+    for i in range(2, len(arr) + 1):
+        yield flip(arr, i)
+
+
+# Zählt, wie viele aufeinanderfolgende Pfannkuchen nebeneinander liegen.
+# Zeitkomplexität: O(n), weil wir nur einmal durch die Liste laufen.
+def count_adj(arr):
+    adj = 0
+    for i in range(1, len(arr)):
+        if arr[i] - arr[i - 1] in (1, -1):
+            adj += 1
+    return adj
+
+
+# Verändert die Zahlen in der Liste so, dass sie in [0, ..., n-1] liegen, wobei die Reihenfolge erhalten bleibt.
+# Zeitkomplexität: O(n^2), weil order.index() O(n) ist und wir n mal aufrufen.
+def normalize(arr):
+    order = sorted(arr)
+    return tuple(order.index(x) for x in arr)
+
+
+# Nähert die minimale Anzahl von flips()s mit count_adj() an.
+# Zeitkomplexität: O(n^2), weil normalize() O(n^2) ist.
+def heuristic(arr):
+    return math.floor((len(arr) - count_adj(normalize(arr)) - 1) / 3)
+
+
+# prüft, ob die Liste in der richtigen Reihenfolge ist.
+# Zeitkomplexität: O(n), weil wir nur einmal durch die Liste laufen.
+def is_sorted(arr):
+    return all(arr[i] <= arr[i + 1] for i in range(len(arr) - 1))
+
+
+# Gibt die Optimale Reihenfolge von flip()s zurück, um die Liste zu sortieren.
+# Zeitkomplexität: ??
+def least_flips(arr):
+    return a_star(normalize(arr), is_sorted, next_arrs, lambda a, b: 1, heuristic)
+
+
+def main():
+    path = input("Pfad: ")
+    with open(path) as f:
+        n_pancakes = int(f.readline())
+        pancakes = tuple(int(x) for x in f.readlines())
+    pancakes = normalize(pancakes)
+    steps = least_flips(pancakes)
+    print(steps)
+    print(len(steps) - 1, "Schritte")
+    print(len(pancakes), "Pfannkuchen")
+    print(heuristic(pancakes), "heuristik")
+    print(math.ceil(len(pancakes) / 1.5), "upper bound")
+    print(len(pancakes) / 2, "lower bound")
+
+    print("-- schritte --")
+    for step in steps:
+        print(count_adj(normalize(step)), "aufeinanderfolgende Pfannkuchen")
+        print(len(step), "Pfannkuchen")
+        print(normalize(step))
+        print(step)
+if __name__ == "__main__":
+    main()        

62fb76c7e3f5140f54d33a442b55bd24c1cd7223
phgilde <ph.gilde@gmail.com>
2023-03-02 22:23:51 +0100

    nicht erneut, wenn lösung bereits existiert

--- weniger-krumme-touren/mip/java_interface.py
+++ weniger-krumme-touren/mip/java_interface.py
@@ -5,6 +5,7 @@ java_path = "weniger-krumme-touren/build/SimulatedAnnealing.jar"
 
 
 def solveTA(path):
-    os.system(f"java -cp weniger-krumme-touren/build touren.SimulatedAnnealing {path}")
+    if not os.path.exists(path + ".solution"):
+        os.system(f"java -cp weniger-krumme-touren/build touren.SimulatedAnnealing {path}")
     with open(path + ".solution") as f:
         return tuple(map(int, f.readline()[1:-1].split(", ")))

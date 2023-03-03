73db1f893504efbd4b6039081097e6bcef26932c
phgilde <ph.gilde@gmail.com>
2023-03-04 00:05:48 +0100

    handling falls null an contraint übergeben wird

--- weniger-krumme-touren/mip/miptour symmetric.py
+++ weniger-krumme-touren/mip/miptour symmetric.py
@@ -47,47 +47,64 @@ def tsp_instance(n: int, c: List[List[int]], points: List[Tuple[int, int]]):
         def generate_constrs(self, model: Model, depth: int = 0, npass: int = 0):
             xf, V_, G = model.translate(self.x), self.V, nx.Graph()
             for (u, v) in [
-                (k, l) for (k, l) in product(V_, V_) if k < l and xf[(k, l)].x > 0.01
+                (k, l) for (k, l) in product(V_, V_) if k < l and xf[(k, l)] is not None
             ]:
-                G.add_edge(u, v, capacity=xf[(u, v)].x)
+                if xf[(u, v)].x > 0.01:
+                    G.add_edge(u, v, capacity=xf[(u, v)].x)
+            if (
+                len(
+                    list(
+                        filter(
+                            lambda e: e[0] in G.nodes and e[1] in G.nodes,
+                            product(V_, V_),
+                        )
+                    )
+                )
+                == 0
+            ):
+                return
             # show the graph
             # nx.draw(G, with_labels=True, pos=points)
             # plt.show()
             try:
                 cycle = nx.algorithms.cycles.find_cycle(G, orientation="ignore")
-                cut = (
-                    xsum(xf[edge(u, v)] for u,v,_ in cycle)
-                    <= len(cycle) - 1
-                )
+                cut = xsum(xf[edge(u, v)] for u, v, _ in cycle) <= len(cycle) - 1
                 model += cut
             except nx.NetworkXNoCycle:
                 pass
-            
+
             if self.use_johnson:
                 # let node1 and node2 be the nodes with highest cost in G between them
                 D = nx.algorithms.shortest_paths.weighted.johnson(G, weight="capacity")
                 node1, node2 = max(
                     product(V_, V_),
-                    key=lambda x: nx.path_weight(G, D[x[0]].get(x[1], [0]), "capacity") if x[0] != x[1] else 0,
+                    key=lambda e: nx.path_weight(G, D[e[0]].get(e[1], [0]), "capacity")
+                    if e[0] != e[1]
+                    else 0,
                 )
             else:
-                node1, node2 = max(product(V_, V_), key=lambda x: c[x[0]][x[1]] if x[0] != x[1] else 0)
-            
+                node1, node2 = max(
+                    filter(
+                        lambda e: e[0] in G.nodes and e[1] in G.nodes, product(V_, V_)
+                    ),
+                    key=lambda e: c[e[0]][e[1]] if e[0] != e[1] else 0,
+                )
 
             # find minimum cut between node1 and node2
             cut_value, (T, TS) = nx.algorithms.flow.minimum_cut(
                 G, node1, node2, capacity="capacity"
             )
             if cut_value < 1 - 1e-6:
-                cut = xsum(xf[edge(u, v)] for u, v in G.subgraph(TS).edges) <= len(G.subgraph(TS).edges) - 1
+                cut = (
+                    xsum(xf[edge(u, v)] for u, v in G.subgraph(TS).edges)
+                    <= len(G.subgraph(TS).edges) - 1
+                )
                 model += cut
-
 
     V = set(range(n))
     Arcs = [(i, j) for (i, j) in product(V, V) if i < j]
 
     model = Model()
-    model.threads = 4
 
     # binary variables indicating if arc (i,j) is used on the route or not
     x = {arc: model.add_var(name=f"Arc {arc}", var_type=BINARY) for arc in Arcs}
@@ -108,8 +125,10 @@ def tsp_instance(n: int, c: List[List[int]], points: List[Tuple[int, int]]):
         f = max(V, key=lambda j: c[i][j])
         F.append((i, f))
 
-    model.cuts_generator = SubTourCutGenerator(x, V, "cuts_generator", c, False)
-    model.lazy_constrs_generator = SubTourCutGenerator(x, V, "lazy_constrs_generator", c, False)
+    model.cuts_generator = SubTourCutGenerator(x, V, c, "cuts_generator", False)
+    model.lazy_constrs_generator = SubTourCutGenerator(
+        x, V, "lazy_constrs_generator", c, False
+    )
     return model, x, ends
 
 
@@ -146,11 +165,11 @@ model, x, ends = tsp_instance(len(points), weight_matrix, points)
 # acute angle constraint
 for i in range(len(points)):
     for j in range(0, len(points)):
-            if i != j:
-                for k in range(0, len(points)):
-                    if i != k and j != k:
-                        if acute(points[i], points[j], points[k]):
-                            model += x[edge(i, j)] + x[edge(j, k)] <= 1
+        if i != j:
+            for k in range(0, len(points)):
+                if i != k and j != k:
+                    if acute(points[i], points[j], points[k]):
+                        model += x[edge(i, j)] + x[edge(j, k)] <= 1
 print("Suche Startlösung...")
 
 init_solution = java_interface.solveTA(path)
@@ -183,11 +202,12 @@ model.emphasis = 2
 model.optimize(max_seconds=main_time * 60)
 print(model.status)
 import winsound
-winsound.MessageBeep()
 
+winsound.MessageBeep()
+print(path)
 if model.status in (OptimizationStatus.OPTIMAL, OptimizationStatus.FEASIBLE):
     print("Lösung gefunden!")
-    
+
     plt.figure(figsize=(10, 10))
     min_coord = min((min([p[0] for p in points]), min([p[1] for p in points])))
     max_coord = max((max([p[0] for p in points]), max([p[1] for p in points])))
